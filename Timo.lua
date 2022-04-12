4aae34da5924d3e86ed5d7dc2fd8c0a4929f7390
EIGEBT <95624954+EIGEBT@users.noreply.github.com>
2022-04-12 04:30:44 +0200

    Add files via upload

--- Timo.lua
+++ Timo.lua
@@ -7152,7 +7152,6 @@ local msgg = msg_id/2097152/0.5
 https.request("https://api.telegram.org/bot"..Token.."/sendphoto?chat_id=" .. msg_chat_id .. "&photo="..photo.photos[1].sizes[#photo.photos[1].sizes].photo.remote.id.."&caption=".. URL.escape(ban_ns).."&reply_to_message_id="..msgg.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(data))
 end
 end
-end
 
 if text == "تست" then
 if Redis:get(Timo.."Status:photo"..msg.chat_id) then
