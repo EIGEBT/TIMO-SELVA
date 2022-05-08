d5fb3e9205fa7a1e30d003e681a0ed97ce8d18c9
EIGEBT <95624954+EIGEBT@users.noreply.github.com>
2022-05-08 16:11:03 +0200

    Add files via upload

--- Timo.lua
+++ Timo.lua
@@ -11626,7 +11626,7 @@ keyboard.inline_keyboard = {
 },
 }
 local rep = msg.id/2097152/0.5
-https.request("https://api.telegram.org/bot"..Token.."/sendaudio?chat_id="..msg_chat_id.."&caption="..URL.escape(t).."&audio="..m.."&reply_to_message_id="..rep.."&parse_mode=Markdown&reply_markup="..JSON.encode(keyboard))
+https.request("https://api.telegram.org/bot"..Token.."/sendaudio?chat_id="..msg_chat_id.."&caption="..URL.escape(t).."&audio="..m.."&reply_to_message_id="..rep.."&parse_mode=Markdown")
 end
 if text == "تويت بالصور" then
 local t = "مرحبا اليك تويت بالصور 🌝💜"
