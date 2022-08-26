153a8db54091bb162befdf879f44fbcb06f3e539
EIGEBT <95624954+EIGEBT@users.noreply.github.com>
2022-08-26 22:10:41 +0200

    Add files via upload

--- Timo.lua
+++ Timo.lua
@@ -18700,6 +18700,76 @@ if text == "زخرفه" or text == "زخرفة" then
     }
 return send(msg_chat_id,msg_id,"*⌯ اختار نوع الزخرفه الان*","md",false ,false ,false ,false ,reply_markup) 
 end
+if Redis:get(Timo.."youtube"..msg.sender_id.user_id..msg_chat_id) == "mp3" then
+Redis:del(Timo.."youtube"..msg.sender_id.user_id..msg_chat_id)
+local rep = msg.id/2097152/0.5
+local m = bot.sendText(msg_chat_id,msg_id,"انتظر قليلا يتم التحميل ...").id
+var(m)
+local se = http.request("http://159.223.13.231/oda/yt?tx="..URL.escape(text))
+local j = JSON.decode(se)
+local link = "http://www.youtube.com/watch?v="..j[1].id
+local title = j[1].title 
+local title = title:gsub("/","-") 
+local title = title:gsub("\n","-") 
+local title = title:gsub("|","-") 
+local title = title:gsub("'","-") 
+local title = title:gsub('"',"-") 
+local d = tostring(j[1].duration)
+local p = j[1].channel
+local p = p:gsub("/","-") 
+local p = p:gsub("\n","-") 
+local p = p:gsub("|","-") 
+local p = p:gsub("'","-") 
+local p = p:gsub('"',"-") 
+print(link)
+print(d)
+os.execute("yt-dlp "..link.." -f 251 -o '"..title..".mp3'")
+bot.sendAudio(msg_chat_id,msg_id,'./'..title..'.mp3',"["..title.."]("..link..")","md",nil,title,p)
+bot.deleteMessages(msg.chat_id,{[1]= m})
+Redis:del(Timo.."youtube"..msg.sender_id.user_id..msg_chat_id)
+sleep(2)
+os.remove(""..title..".mp3")
+end
+if Redis:get(Timo.."youtube"..msg.sender_id.user_id..msg_chat_id) == "mp4" then
+local rep = msg.id/2097152/0.5
+local m = bot.sendText(msg_chat_id,msg_id,"انتظر قليلا يتم التحميل ...").id
+local se = http.request("http://159.223.13.231/oda/yt?tx="..URL.escape(text))
+local j = JSON.decode(se)
+local link = "http://www.youtube.com/watch?v="..j[1].id
+local title = j[1].title 
+local title = title:gsub("/","-") 
+local title = title:gsub("\n","-") 
+local title = title:gsub("|","-") 
+local title = title:gsub("'","-") 
+local title = title:gsub('"',"-") 
+local d = tostring(j[1].duration)
+local p = j[1].channel
+local p = p:gsub("/","-") 
+local p = p:gsub("\n","-") 
+local p = p:gsub("|","-") 
+local p = p:gsub("'","-") 
+local p = p:gsub('"',"-") 
+print(d)
+os.execute("yt-dlp "..link.." -f 18 -o '"..title..".mp4'")
+local s = bot.sendVideo(msg_chat_id,msg_id,'./'..title..'.mp4',"["..title.."]("..link..")","md")
+bot.deleteMessages(msg.chat_id,{[1]= m})
+Redis:del(Timo.."youtube"..msg.sender_id.user_id..msg_chat_id)
+sleep(2)
+os.remove(""..title..".mp4")
+end
+if text == "يوتيوب" then
+local reply_markup = bot.replyMarkup{
+type = 'inline',
+data = {
+{
+{text = 'تحميل صوت', data = msg.sender_id.user_id..'/mp3'..msg_id}, {text = 'تحميل فيديو', data = msg.sender_id.user_id..'/mp4'..msg_id}, 
+},
+}
+}
+return send(msg_chat_id,msg_id, [[*
+⌯ اختر كيف تريد التحميل
+*]],"md",false, false, false, false, reply_markup)
+end
 ------الردود--- 
 if text == ("مسح الردود الانلاين") then
 if not msg.Manger then
